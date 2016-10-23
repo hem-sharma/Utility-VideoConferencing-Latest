d77512d44363ddde3ccc9ec782ff87026c577869
kaushik-hemant <kaushik.hemant@live.com>
2016-10-23 20:51:16 +0530

    service bug fixed

--- kurento-room-demo/src/main/resources/static/angular/services/authorizeService.js
+++ kurento-room-demo/src/main/resources/static/angular/services/authorizeService.js
@@ -1,20 +1,20 @@
-kurento_room.factory('authService',function ($http,$q) {
-    var service={};
+kurento_room.factory('authService', ['$http', '$q', function ($http, $q) {
+    var service = {};
     service.auth = auth;
     return service;
-    
-    function auth(event,token) {
-        var data={
-            Event:event,
-            Token:token
+
+    function auth(event, token) {
+        var data = {
+            Event: event,
+            Token: token
         };
         var deferred = $q.defer();
-        $http.get('http://52.187.79.197:85/api/room/authorize',data).then(function (res) { 
+        $http.get('http://52.187.79.197:85/api/room/authorize', data).then(function (res) {
             deferred.resolve(res);
-         })
-         .then(function(res){
-             deferred.reject(res);
-         });
-         return deferred.promise;
-      }
-});
+        })
+            .then(function (res) {
+                deferred.reject(res);
+            });
+        return deferred.promise;
+    }
+}]);
