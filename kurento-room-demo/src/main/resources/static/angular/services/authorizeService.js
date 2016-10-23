9817e810fae7fb3b8d3161f7084e2d6ccdc908fc
kaushik-hemant <kaushik.hemant@live.com>
2016-10-23 20:56:12 +0530

    test

--- kurento-room-demo/src/main/resources/static/angular/services/authorizeService.js
+++ kurento-room-demo/src/main/resources/static/angular/services/authorizeService.js
@@ -1,20 +1,17 @@
-kurento_room.factory('authService', ['$http', '$q', function ($http, $q) {
+kurento_room.factory('authService', ['$http', '$q', 
+function ($http, $q) {
     var service = {};
     service.auth = auth;
     return service;
 
-    function auth(event, token) {
-        var data = {
-            Event: event,
-            Token: token
-        };
+    function auth() {
         var deferred = $q.defer();
-        $http.get('http://52.187.79.197:85/api/room/authorize', data).then(function (res) {
+        $http.get('http://52.187.79.197:85/api/room/authorize').then(function (res) {
             deferred.resolve(res);
         })
             .then(function (res) {
                 deferred.reject(res);
             });
         return deferred.promise;
-    }
+    };
 }]);
