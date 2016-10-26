a316c0e0a35284c99109818b96691becd49f37e5
kaushik-hemant <kaushik.hemant@live.com>
2016-10-26 17:32:55 +0530

    test

--- kurento-room-demo/src/main/resources/static/angular/app.js
+++ kurento-room-demo/src/main/resources/static/angular/app.js
@@ -23,7 +23,9 @@ kurento_room.config(function ($routeProvider) {
 
 kurento_room.filter('split', function () {
     return function (input, splitChar, splitIndex) {
-        return input.split(splitChar)[splitIndex];
+        if (!input)
+            return input.split(splitChar)[splitIndex];
+        return '';
     }
 });
 
