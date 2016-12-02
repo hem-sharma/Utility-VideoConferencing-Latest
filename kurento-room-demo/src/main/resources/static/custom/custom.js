ccc8bf28f5e473aac65c3996ae93a74cd93a9071
kaushik-hemant <kaushik.hemant@live.com>
2016-12-02 12:46:16 +0530

    UI changes

--- kurento-room-demo/src/main/resources/static/custom/custom.js
+++ kurento-room-demo/src/main/resources/static/custom/custom.js
@@ -16,3 +16,16 @@ function showPopUp() {
 function hidePopUp() {
     $('#popUp').css('display', 'none')
 }
+
+document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key == "Escape" || evt.key == "Esc");
+    } else {
+        isEscape = (evt.keyCode == 27);
+    }
+    if (isEscape) {
+        hidePopUp();
+    }
+};
